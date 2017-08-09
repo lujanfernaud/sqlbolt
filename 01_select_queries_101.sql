2cd9e4c0b8d296fef1368cf300f13e18eefea055
lujanfernaud <lujanfernaud@gmail.com>
2017-08-09 22:46:01 +0100

    Add exercises for lesson 1

---
+++ 01_select_queries_101.sql
@@ -0,0 +1,24 @@
+-- 1. Find the title of each film.
+
+SELECT title
+  FROM movies;
+
+-- 2. Find the director of each film.
+
+SELECT director
+  FROM movies;
+
+-- 3. Find the title and director of each film.
+
+SELECT title, director
+  FROM movies;
+
+-- 4. Find the title and year of each film.
+
+SELECT title, year
+  FROM movies;
+
+-- 5. Find all the information about each film.
+
+SELECT *
+  FROM movies;
