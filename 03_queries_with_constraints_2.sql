2b12df5fb72f5384cf95894dd72ce31fa8cebe6b
lujanfernaud <lujanfernaud@gmail.com>
2017-08-09 23:03:21 +0100

    Add exercises for lesson 3

---
+++ 03_queries_with_constraints_2.sql
@@ -0,0 +1,23 @@
+-- 1. Find all the Toy Story movies.
+
+SELECT title
+  FROM movies
+ WHERE title LIKE 'Toy Story%';
+
+-- 2. Find all the movies directed by John Lasseter.
+
+SELECT title
+  FROM movies
+ WHERE director = 'John Lasseter';
+
+-- 3. Find all the movies (and director) not directed by John Lasseter.
+
+SELECT title
+  FROM movies
+ WHERE director != 'John Lasseter';
+
+ -- 4. Find all the WALL-* movies.
+
+ SELECT title
+   FROM movies
+  WHERE title LIKE 'WALL-%';
