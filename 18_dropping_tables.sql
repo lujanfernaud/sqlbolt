ac16f2f2968763c538fb1553efad5ee3b48a704b
lujanfernaud <lujanfernaud@gmail.com>
2017-08-10 14:19:24 +0100

    Add exercises for lesson 18

---
+++ 18_dropping_tables.sql
@@ -0,0 +1,8 @@
+-- 1. We've sadly reached the end of our lessons, lets clean up by removing
+-- the Movies table.
+
+DROP TABLE IF EXISTS movies;
+
+-- 2. And drop the BoxOffice table as well.
+
+DROP TABLE IF EXISTS boxoffice;
